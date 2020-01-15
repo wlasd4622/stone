d065fe83a7082877179adc6eb240730f6e77c8af
王文龙 <357934522@qq.com>
2020-01-15 15:09:30 +0800

    .

--- tao_v2/assets/js/taobao.js
+++ tao_v2/assets/js/taobao.js
@@ -9,7 +9,24 @@ let time = setInterval(_ => {
       $(document).on('click', '.detectionVideo', function () {
         localStorage.setItem('wwlTask', true);
       }).on('click', '.wwlplay', function () {
-        console.log(6666);
+        let audioContainer = $('.audioContainer')
+        if (!audioContainer) {
+          audioContainer = $('<div>')
+          audioContainer.addClass('audioContainer')
+          audioContainer.append(`
+            <div class="wrap">
+              <div class="c_close"><button class="close">close</button></div>
+              <div class="c_liframe">
+                <iframe></iframe>
+              </div>
+            </div>
+          `)
+          $('body').append(audioContainer)
+
+          audioContainer.find('iframe').attr('src', 'http://vjs.zencdn.net/v/oceans.mp4')
+        }
+      }).on('click', '.c_close', function () {
+        $('.audioContainer').remove();
       })
     }
   } catch (err) {
